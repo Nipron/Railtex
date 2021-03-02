ea50ca2c07120f8e7d5e967ec287fb7a7d6fcc1f
Vitalii Doroshenko <2328221@ukr.net>
2021-03-02 04:36:29 +0200

    header styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <ul>
+                <li className="step1">
+                    CONTACTS
+                </li>
+                <li className="step1">
+                    ADRESS
+                </li>
+                <li className="step1">
+                    CATEGORIES
+                </li>
+                <li className="step1">
+                    SUCCESS
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
